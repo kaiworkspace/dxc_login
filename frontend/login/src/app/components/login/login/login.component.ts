90a80903cdb430d70978df16888708597454bfdb
Kai Heng <johnloh1998@gmail.com>
2024-03-10 00:52:07 +0800

    initialised login component and service

---
+++ frontend/login/src/app/components/login/login/login.component.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { LoginService } from './login.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private loginService: LoginService) { }
+
+  ngOnInit(): void {
+  }
+
+  loginForm = new FormGroup({
+    username: new FormControl(""),
+    password: new FormControl("")
+  })
+
+
+  login(){
+    this.loginService.login()
+    console.log("Logging user in")
+  }
+}
