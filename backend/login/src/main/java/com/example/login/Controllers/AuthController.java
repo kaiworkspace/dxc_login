d7fc123c2abc2218d6c789ed162aa06bc2b7008b
Kai Heng <johnloh1998@gmail.com>
2024-03-10 00:00:39 +0800

    added user login

--- backend/login/src/main/java/com/example/login/Controllers/AuthController.java
+++ backend/login/src/main/java/com/example/login/Controllers/AuthController.java
@@ -9,6 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -18,6 +22,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -77,6 +82,42 @@ public class AuthController {
 		
 		return new ResponseEntity<>("User registered successfully", HttpStatus.OK);
 	}
+	
+	@CrossOrigin
+	@GetMapping("/login")
+	public ResponseEntity<String> login(@RequestHeader(value="Authorization", required=true) String authorizationHeader){
+		// check auth header
+		if(authorizationHeader == null && !authorizationHeader.startsWith("Basic")) {
+			return new ResponseEntity<>("Missing or invalid headers", HttpStatus.BAD_REQUEST);
+		}
+		
+		// extract and decode
+		String credentialBased64 = authorizationHeader.substring(6);
+		String decodedCredentials = new String(java.util.Base64.getDecoder().decode(credentialBased64));
+		String[] credentialsArr = decodedCredentials.split(":");
+		String username = "";
+		String password = "";
+		
+		// check if both username and password is present
+		try {
+			username = credentialsArr[0];
+			password = credentialsArr[1];
+		}catch(Exception e) {
+			return new ResponseEntity<>("Missing Credentials", HttpStatus.BAD_REQUEST);
+		}
+		
+		// check if password matches
+		try {
+			Authentication authentication = authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(username, password));
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+			
+			return new ResponseEntity<>("User signed in successfully", HttpStatus.OK);
+		}catch(BadCredentialsException e) {
+			return new ResponseEntity<>("Invalid username or password", HttpStatus.UNAUTHORIZED);
+		}
+	}
+	
 	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException e){
