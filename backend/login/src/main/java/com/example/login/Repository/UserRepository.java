1c105db4c48bb578aff7ad2a680894560bc7fe7f
Kai Heng <johnloh1998@gmail.com>
2024-03-09 22:52:48 +0800

    added custom user detail service and auth manager to security config

--- backend/login/src/main/java/com/example/login/Repository/UserRepository.java
+++ backend/login/src/main/java/com/example/login/Repository/UserRepository.java
@@ -2,12 +2,13 @@ package com.example.login.Repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.login.Models.UserEntity;
 
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<UserEntity, Integer>{
 	
 	Optional<UserEntity> findByUsername(String username);
 	
--- backend/login/src/main/java/com/example/login/Security/SecurityConfiguration.java
+++ backend/login/src/main/java/com/example/login/Security/SecurityConfiguration.java
@@ -1,14 +1,29 @@
 package com.example.login.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+	
+	private CustomUserDetailsService userDetailsService;
+	private AuthenticationConfiguration authenticationConfiguration;
+	
+	@Autowired
+	public SecurityConfiguration(CustomUserDetailsService userDetailsService, 
+			AuthenticationConfiguration authenticationConfiguration) {
+		this.userDetailsService = userDetailsService;
+		this.authenticationConfiguration = authenticationConfiguration;
+	}
 	
 	// configure filter chain
 	@Bean
@@ -21,5 +36,15 @@ public class SecurityConfiguration {
 					.anyRequest().authenticated()
 					);
 		return http.build();
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
